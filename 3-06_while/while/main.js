9c1a84e03919beff99752420a14dfc3def6a02c6
kazu16916 <130880184+kazu16916@users.noreply.github.com>
2023-06-23 16:29:29 +0900

    0623_js_class

---
+++ 3-06_while/while/main.js
@@ -0,0 +1,21 @@
+//while文での繰り返し
+let i = 1;
+while (i <=10){
+    console.log(i)
+    i +=1
+}
+
+//for文での繰り返し
+for (let f = 1;f <= 10; f++){
+    console.log(f)
+}
+
+
+let y = 1;
+while (y <= 30){
+    console.log(y)
+    y+=1
+}
+for (e = 10;e <= 100;e++){
+    console.log('あなたのお金は'+e+'兆円です')
+}
---
+++ js_practice/0623/main_2.js
@@ -0,0 +1,21 @@
+var type=['ドラゴン','ゴースト','じめん','どく','むし','みず','でんき','いわ','くさ','あく','こおり','ノーマル','ほのお','かくとう','はがね','エスパー','フェアリー'];
+const you_number = Math.floor(Math.random()*18);
+const you = type[you_number];
+const me = (window.prompt('タイプを入力してください'));
+let message;
+if (you == ''&&me == '') {
+    message = ''
+}else if (you == ''&&me == ''){
+    message = ''
+}else if (you ==''&&me == ''){
+    message = ''
+}else if (you == ''&&me== ''){
+    message = ''
+}else if (you == ''&&me == ''){
+    message = ''
+}else if (you == ''&&me == ''){
+    message = ''
+}else if (you == ''&&me== ''){
+    message = ''
+}
+window.alert(message);
